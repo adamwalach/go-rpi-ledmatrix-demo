167cb8c70496e31dd17cb73e0a5cdf51a17268b6
Adam Wałach <adam.walach@sap.com>
2016-05-06 10:56:24 +0200

    build fix

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 go version
-GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=0 go build -o=main *.go
+CC_PATH=/usr/bin/arm-linux-gnueabi-gcc
+CXX_PATH=/usr/bin/arm-linux-gnueabi-cpp
+
+CC=$CC_PATH CXX=CXX_PATH GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=1 go build -o=main *.go
