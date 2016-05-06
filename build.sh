900a338efe6a4a2a0096121d47bc0d2a45998881
Adam Wałach <adam.walach@sap.com>
2016-05-06 10:59:52 +0200

    sdas

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ go version
 CC_PATH=/usr/bin/arm-linux-gnueabi-gcc
 CXX_PATH=/usr/bin/arm-linux-gnueabi-cpp
 
-CC=$CC_PATH CXX=CXX_PATH GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=1 go build -o=main *.go
+CC=$CC_PATH CXX=$CXX_PATH GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=1 go build -o=main *.go
